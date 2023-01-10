44ab481433fbf4e5414d98434871c64832403d1a
ry10an23 <kawaguchi.r823@gmail.com>
2023-01-09 23:09:00 -0800

    add scrolling event and side menu code

--- exhibition/index.js
+++ exhibition/index.js
@@ -22,65 +22,76 @@ function hamburgerMenu() {
 }
 
 /*=================================================
+    SCROLLING EVENT
+===================================================*/
+window.addEventListener("scroll", function () {
+  let scroll = window.pageYOffset;
+  mv_scale(scroll);
+
+  /*=================================================
     SHOW THE MENU BAR
 ===================================================*/
 
-const logo = document.getElementsByClassName("logo");
-const hamburger = document.getElementsByClassName("hamburger");
+  const logo = document.getElementsByClassName("logo");
+  const hamburger = document.getElementsByClassName("hamburger");
 
-window.addEventListener("scroll", () => {
-  const currentY = window.pageYOffset;
-  if (currentY > 520) {
-    setTimeout(function () {
-      logo[0].style.display = "block";
-      hamburger[0].style.display = "block";
-    }, 1);
-  } else {
-    setTimeout(function () {
-      logo[0].style.display = "none";
-      hamburger[0].style.display = "none";
-    }, 1);
-  }
-});
-
-/*=================================================
-    RESCALING MAIN VISUAL IMAGES
-===================================================*/
-
-window.addEventListener("scroll", () => {
-  if (window.innerWidth > 900) {
+  window.addEventListener("scroll", () => {
     const currentY = window.pageYOffset;
-    let imgs = document.getElementsByClassName("mainVisImg");
-    for (const img of imgs) {
-      img.style.width = 100 / 3 + currentY / 10 + "%";
+    if (currentY > 520) {
+      setTimeout(function () {
+        logo[0].style.display = "block";
+        hamburger[0].style.display = "block";
+      }, 1);
+    } else {
+      setTimeout(function () {
+        logo[0].style.display = "none";
+        hamburger[0].style.display = "none";
+      }, 1);
     }
-  } else {
-    const currentY = window.pageYOffset;
-    let imgs = document.getElementsByClassName("mainVisImg");
-    for (const img of imgs) {
-      img.style.width = 100 - currentY / 10 + "%";
-    }
-  }
-});
+  });
 
-/*=================================================
+  /*=================================================
     FADE IN GALLERY IMAGES
 ===================================================*/
-let galleryImgs = document.querySelectorAll(".pic");
+  let galleryImgs = document.querySelectorAll(".pic");
 
-window.addEventListener("scroll", () => {
-  let scroll = window.scrollY; // Get the scroll area
-  let windowHeight = window.innerHeight; //Get the height of window without bookmark bar
-  for (let galleryImg of galleryImgs) {
-    let targetPos = galleryImg.getBoundingClientRect().top + scroll;
-    if (scroll > targetPos - windowHeight) {
-      if (galleryImg.classList.contains("fadein")) {
-        galleryImg.classList.add("show");
+  window.addEventListener("scroll", () => {
+    let scroll = window.scrollY; // Get the scroll area
+    let windowHeight = window.innerHeight; //Get the height of window without bookmark bar
+    for (let galleryImg of galleryImgs) {
+      let targetPosImg = galleryImg.getBoundingClientRect().top + scroll;
+      if (scroll > targetPosImg - windowHeight) {
+        if (galleryImg.classList.contains("fadein")) {
+          galleryImg.classList.add("show");
+        }
       }
     }
+  });
+
+  /*=================================================
+    SHOW SIDE BUTTON
+===================================================*/
+  let gallery = document.getElementById("gallery");
+  let gallery_rect = gallery.getBoundingClientRect();
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  let gallery_position = gallery_rect.top + scrollTop;
+
+  let access = document.getElementById("access");
+  let access_rect = access.getBoundingClientRect();
+  let access_position = access_rect.top + scrollTop;
+
+  let sideBtn = document.getElementById("side_btn");
+
+  if (window.innerWidth > 900) {
+    if (scroll > gallery_position) {
+      if (scroll < access_position) {
+        // console.log("hello world");
+        sideBtn.setAttribute("id", "side_btn_show");
+      } else {
+        sideBtn.setAttribute("id", "side_btn");
+      }
+    }
+  } else {
+    sideBtn.setAttribute("id", "side_btn");
   }
 });
-
-/*=================================================
-    SHOW THE BACK GROUND IMAGE
-===================================================*/
